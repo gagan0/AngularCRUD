1cade2471728053b4da20ba7df0845dee19b00c9
GAGAN\Vawsum <ggm@vawsum.com>
2019-08-18 04:24:12 +0530

    Changes continued

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+
+const appRoutes: Routes =
+	[
+		{
+			path: "list",
+			component: ListEmployeesComponent
+		},
+		{
+			path: "create",
+			component: CreateEmployeeComponent
+		},
+		{
+			path: "",
+			redirectTo: "/list",
+			pathMatch: "full"
+		}
+	];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListEmployeesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ListEmployeesComponent,
+		CreateEmployeeComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		RouterModule.forRoot(appRoutes)
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
